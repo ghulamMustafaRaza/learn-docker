b2c82707929e07f684502a3fc095f71604b48bc8
Ghulam Mustafa Raza Qadri <1q2w3e.gm@gmail.com>
2018-10-17 12:03:01 +0500

    Create docker_command.sh

---
+++ docker_command.sh
@@ -0,0 +1,43 @@
+docker version     # Show the Docker version information
+docker info        # Display system-wide information
+docker pull        # Pull an image or a repository from a registry
+docker images      # List images
+
+docker run         # Run a command in a new container
+docker ps          # List containers
+
+docker rmi         # Remove one or more images
+docker rm          # Remove one or more containers
+
+docker attach      # Attach local standard input, output, and error streams to a running container
+docker build       # Build an image from a Dockerfile
+docker commit      # Create a new image from a container's changes
+docker cp          # Copy files/folders between a container and the local filesystem
+docker create      # Create a new container
+docker diff        # Inspect changes to files or directories on a container's filesystem
+docker events      # Get real time events from the server
+docker exec        # Run a command in a running container
+docker export      # Export a container's filesystem as a tar archive
+docker history     # Show the history of an image
+docker import      # Import the contents from a tarball to create a filesystem image
+docker inspect     # Return low-level information on Docker objects
+docker kill        # Kill one or more running containers
+docker load        # Load an image from a tar archive or STDIN
+docker login       # Log in to a Docker registry
+docker logout      # Log out from a Docker registry
+docker logs        # Fetch the logs of a container
+docker pause       # Pause all processes within one or more containers
+docker port        # List port mappings or a specific mapping for the container
+docker push        # Push an image or a repository to a registry
+docker rename      # Rename a container
+docker restart     # Restart one or more containers
+docker save        # Save one or more images to a tar archive (streamed to STDOUT by default)
+docker search      # Search the Docker Hub for images
+docker start       # Start one or more stopped containers
+docker stats       # Display a live stream of container(s) resource usage statistics
+docker stop        # Stop one or more running containers
+docker tag         # Create a tag TARGET_IMAGE that refers to SOURCE_IMAGE
+docker top         # Display the running processes of a container
+docker unpause     # Unpause all processes within one or more containers
+docker update      # Update configuration of one or more containers
+docker wait        # Block until one or more containers stop, then print their exit codes
